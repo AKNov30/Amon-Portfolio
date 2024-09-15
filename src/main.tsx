95b72e63939b15b9df968cd2f896dd1c2d5c23ec
AKNov30 <amonthjp@gmail.com>
2024-09-16 06:52:00 +0700

    feat: add Navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import WrappedApp from './components/WrappedApp'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <WrappedApp></WrappedApp>
   </StrictMode>,
 )
